faadf57edc31ea2f358358e0790dcef1250da94a
Ana Oliveira <aclarafernandes123@gmail.com>
2024-05-14 15:49:06 -0300

    commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textNome}> Ana Clara Fernandes de Oliveira</Text>
+      <Text style={styles.textNumero}> Número 02</Text>
+      <Text style={styles.textSala}> Sala 07</Text>
+      <Text style={styles.textElemento}> Recuperação</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  textNome: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "blue",
+    textAlign: "right",
+  },
+  textNumero: {
+    fontStyle: "italic",
+    fontSize: 25,
+    borderBottomWidth: 1,
+  },
+  textSala: {
+    fontSize: 20,
+    textAlign: "center",
+    backgroundColor: "pink",
+  },
+  textElemento: {
+    fontSize: 20,
+    backgroundColor: "gray",
+    borderBottomWidth: 1,
   },
 });
